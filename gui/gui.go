3af3d9545d07611cd900f14f578c8cd4b3c3b3a8
Kyle Reichert <kyle@kyle.com>
2026-01-15 19:47:31 -0700

    Implement basic textbox and refactor

--- gui/gui.go
+++ gui/gui.go
@@ -1,16 +1,38 @@
 package gui
 
 import (
+	"log"
+	// "time"
 	// "fyne.io/fyne/container"
 	"fyne.io/fyne/v2"
 	// "fyne.io/fyne/v2/app"
-	// "fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
-func Run(app fyne.App) {
-	w := app.NewWindow("Hello")
+type Size struct {
+	Width  float32
+	Height float32
+}
 
-	w.SetContent(widget.NewLabel("Hello, Again!"))
+var mainSize Size = Size{
+	Width:  1000,
+	Height: 800,
+}
+
+func Run(app fyne.App) {
+	w := app.NewWindow("Hello World")
+	w.SetContent(widget.NewLabel("Hello World"))
+	w.Resize(fyne.NewSize(mainSize.Width, mainSize.Height))
+
+	usjInput := widget.NewEntry()
+	usjInput.SetPlaceHolder("USJ: ")
+
+	content := container.NewVBox(usjInput, widget.NewButton("Save", func() {
+		log.Println("Content was:", usjInput.Text)
+	}))
+
+	w.SetContent(content)
 	w.ShowAndRun()
+	// app.Run()
 }
